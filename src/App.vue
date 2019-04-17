4a807d2e256475bea58002255cde603be1002222
omar <you@example.com>
2019-04-17 19:58:56 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+  <p>{{ count }}</p>
+<p>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+</p>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  computed: {
+      count () {
+  	    return this.$store.state.count
+      }
+    },
+    methods: {
+      increment () {
+        this.$store.commit('increment')
+      },
+      decrement () {
+      	this.$store.commit('decrement')
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
